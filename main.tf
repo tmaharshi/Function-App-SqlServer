946e4419b599ae7919991aa531be5490d6758335
tmaharshi <88552424+tmaharshi@users.noreply.github.com>
2025-01-24 16:18:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
 
 module "resource_group" {
   source              = "./modules/resource_group"
-  resource_group_name = var.resource_group_name
+  resource_group_name = module.resource_group.resource_group_name
   location            = var.location
 }
 
